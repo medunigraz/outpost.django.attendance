6686a73e81e746f0967264f7d15ba24877fd550b
Michael Fladischer <michael@fladi.at>
2019-11-05 11:29:30 +0100

    Allow expansion of entries.student for CampusOnlineHolding serializer.

--- src/outpost/django/attendance/api.py
+++ src/outpost/django/attendance/api.py
@@ -32,7 +32,7 @@ class CampusOnlineHoldingViewSet(FlexFieldsMixin, viewsets.ModelViewSet):
     filter_class = filters.CampusOnlineHoldingFilter
     ordering_fields = ("initiated",)
     permission_classes = (permissions.IsAuthenticated,)
-    permit_list_expands = ("entries", "course_group_term")
+    permit_list_expands = ("entries", "entries.student", "course_group_term")
     http_method_names = viewsets.ModelViewSet.http_method_names + [
         "start",
         "end",
